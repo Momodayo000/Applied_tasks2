8f62ab15015ff8addfa25a9aa4ff09055e650bc3
EC2 Default User <ec2-user@ip-172-31-34-225.ap-northeast-1.compute.internal>
2023-09-26 06:45:55 +0000

    2023/09/26_assignment5_fin

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,20 @@ class User < ApplicationRecord
     following_users.include?(user)
   end
 
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
